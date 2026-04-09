e3fd59f801a0056b611673bf6bef1e5b1df1a1b9
Coolhubert <1backup5acc@gmail.com>
2026-04-09 22:42:21 +0000

    new fix

--- public/index.js
+++ public/index.js
@@ -62,3 +62,8 @@ form.addEventListener("submit", async (event) => {
 	document.body.appendChild(frame.frame);
 	frame.go(url);
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});

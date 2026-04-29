4bbc919d8b686f14a00ced2fc480fa6eaaea7dd4
CoolHubert <1backup5acc@gmail.com>
2026-04-28 20:58:40 -0500

    new backgroud

--- public/index.js
+++ public/index.js
@@ -284,3 +284,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+document.addEventListener("mousemove", (e) => {
+    document.body.style.setProperty("--x", e.clientX + "px");
+    document.body.style.setProperty("--y", e.clientY + "px");
+});
